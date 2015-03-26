f7cecc204fc6623a1d44a6a38719efcd98d0a0ad
gerand <gerand@7bab9ee4-61d2-4ee8-b121-30142f00ee79>
2015-03-26 01:02:56 +0000

    basicintegrationtest: wait until verticle is deployed, tests: add log4j config

--- gameregistry/src/test/resources/integration_tests/groovy/BasicIntegrationTest.groovy
+++ gameregistry/src/test/resources/integration_tests/groovy/BasicIntegrationTest.groovy
@@ -11,17 +11,21 @@ import es.us.dad.gameregistry.ToyRestServer
 
 def testRestServer() {
     // deploy only the ToyRestServer
-    container.deployVerticle("groovy:" + ToyRestServer.class.getName())
+    container.deployVerticle("groovy:" + ToyRestServer.class.getName(), { asyncResult ->
+        assertTrue(asyncResult.succeeded)
+        assertNotNull("deploymentID should not be null", asyncResult.result())
 
-    vertx.createHttpClient().setPort(1080).getNow("/", { HttpClientResponse resp ->
-        assertEquals(200, resp.statusCode)
+        // now the verticle is deployed -> test it
+        vertx.createHttpClient().setPort(1080).getNow("/", { HttpClientResponse resp ->
+            assertEquals(200, resp.statusCode)
 
-        resp.bodyHandler { Buffer content ->
-            assertEquals("Hello world.", content.toString())
+            resp.bodyHandler { Buffer content ->
+                assertEquals("Hello world.", content.toString())
 
-            // tests are async, we have to notify the test runner that the test has finished
-            testComplete()
-        }
+                // tests are async, we have to notify the test runner that the test has finished
+                testComplete()
+            }
+        })
     })
 }
 
