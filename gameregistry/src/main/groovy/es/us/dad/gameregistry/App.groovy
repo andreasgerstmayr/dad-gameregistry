0103bd5130ce255c38d7f979943ac57be62dd2ac
rafgalgal1 <rafgalgal1@7bab9ee4-61d2-4ee8-b121-30142f00ee79>
2015-04-05 01:44:50 +0000

    Started integration of mongo-persistor.
    * Dockerfile now copies conf-docker.json as conf.json into the container.
      The CMD in the Dockerfile now passes that conf.json to vertx as configuration.
      Also the mongo-persistor module is preinstalled along the groovy support platform.
    
    * Added mongo-persistor as a dependency to build.gradle.
    
    * Added basic configuration to conf.json and conf-docker.json to
      connect to a mongo server. The normal conf.json tries to connect
      to a mongo server in localhost while conf-docker expects to find
      a running instance in a host named 'mongo-server'.
    
    * Updated App.groovy to deploy the module mongo-persistor.
    
    * Updated README.md. Describes how to launch a mongo docker container
      in a way that links to our container allowing connections between them.

--- gameregistry/src/main/groovy/es/us/dad/gameregistry/App.groovy
+++ gameregistry/src/main/groovy/es/us/dad/gameregistry/App.groovy
@@ -6,10 +6,11 @@ import org.vertx.groovy.platform.Verticle
 class App extends Verticle {
 
     def start() {
+		def mongoConfig = container.getConfig().'mongo-persistor'
 
-        // start all verticles
-
-
+        // start all verticles or modules
+		container.deployModule("io.vertx~mod-mongo-persistor~2.1.1", mongoConfig)
+		
         // java toyrestserver
         container.deployVerticle(ToyRestServer.class.getName())
 
