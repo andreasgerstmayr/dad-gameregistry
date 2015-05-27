7c25d1f9f7869ad5c2f3b581a88096ecab65bd2a
Andreas Gerstmayr <andreas.gerstmayr@gmail.com>
2015-05-27 22:28:26 +0200

    refactor app start using promises

--- gameregistry-client-example/src/main/java/es/us/dad/gameregistry/shared/domain/DomainObject.java
+++ gameregistry-client-example/src/main/java/es/us/dad/gameregistry/shared/domain/DomainObject.java
@@ -1,34 +1,31 @@
 package es.us.dad.gameregistry.shared.domain;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
 
-abstract class DomainObject {
+abstract public class DomainObject {
 
-    final static private DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
-
-    public static String formatDate(Date date) {
+    public static Long formatDate(Date date) {
         if (date == null)
             return null;
         else
-            return dateFormat.format(date);
+            return date.getTime();
     }
 
     public static Date toDate(Object date) {
         try {
-            return dateFormat.parse(date.toString());
-        } catch (NullPointerException | ParseException ignore) {
+            return new Date(Long.parseLong(date.toString()));
+        } catch (NullPointerException | NumberFormatException ignore) {
             return null;
         }
     }
 
     public DomainObject() {
     }
+
     public DomainObject(Map<String, Object> jsonMap) {
     }
+
     abstract public Map<String, Object> toJsonMap();
 
 }
--- gameregistry/src/main/groovy/es/us/dad/gameregistry/server/App.groovy
+++ gameregistry/src/main/groovy/es/us/dad/gameregistry/server/App.groovy
@@ -1,39 +1,42 @@
 package es.us.dad.gameregistry.server
 
+import com.darylteo.vertx.promises.groovy.Promise
 import org.vertx.groovy.platform.Verticle
 import org.vertx.java.core.Future
 
 
 class App extends Verticle {
 
-    def startMongoVerticle(Map<String, Object> mongoConfig, Closure deployResult) {
+    Promise<Void> startMongoVerticle(Map<String, Object> mongoConfig) {
 		def mod_mongo = "io.vertx~mod-mongo-persistor~2.1.1"
+        Promise<Void> p = new Promise<Void>()
 		
 		container.logger.info("Deploying module: ${mod_mongo}")
         // deploy mongodb persistor
         container.deployModule(mod_mongo, mongoConfig, { asyncResult ->
-            if(asyncResult.failed) {
-                container.logger.error("Can't deploy mongo db persistor:")
-                container.logger.error(asyncResult.cause())
-            }
-
-            deployResult.call(asyncResult)
+            if(asyncResult.failed)
+                p.reject(asyncResult.cause())
+            else
+                p.fulfill(null)
         })
+
+        return p
     }
 
-    def startRestServer(Map<String, Object> gameRegistryConfig, Closure deployResult) {
+    def startRestServer(Map<String, Object> gameRegistryConfig) {
 		def verticle_rest = "groovy:" + RestServer.class.getName()
-		
+        Promise<Void> p = new Promise<Void>()
+
 		container.logger.info("Deploying GameRegistry REST server (${verticle_rest})...")
         // deploy groovy test rest server
         container.deployVerticle(verticle_rest, gameRegistryConfig, { asyncResult ->
-            if(asyncResult.failed) {
-                container.logger.error("Can't deploy ${verticle_rest}:")
-                container.logger.error(asyncResult.cause())
-            }
-
-            deployResult.call(asyncResult)
+            if(asyncResult.failed)
+                p.reject(asyncResult.cause())
+            else
+                p.fulfill(null)
         })
+
+        return p
     }
 
     def start(Future<Void> startedResult) {
@@ -43,18 +46,14 @@ class App extends Verticle {
         Map<String, Object> mongoConfig = appConfig.getOrDefault("mongo-persistor", [:]) as Map<String, Object>
         Map<String, Object> gameRegistryConfig = appConfig.getOrDefault("game-registry", [:]) as Map<String, Object>
 
-        startMongoVerticle(mongoConfig, { asyncResult2 ->
-            if (asyncResult2.failed) {
-                startedResult.setFailure(asyncResult2.result())
-            }
-            else {
-                startRestServer(gameRegistryConfig, { asyncResult3 ->
-                    if (asyncResult3.failed)
-                        startedResult.setFailure(asyncResult3.result())
-                    else
-                        startedResult.setResult(null)
-                })
-            }
+        startMongoVerticle(mongoConfig).then({
+            return startRestServer(gameRegistryConfig)
+        }).then({
+            startedResult.setResult(null)
+        }).fail({Exception ex ->
+            container.logger.error("Error when starting app:")
+            container.logger.error(ex)
+            startedResult.setFailure(ex)
         })
     }
 }
